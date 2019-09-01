08e3dd5a5507e9e75adc19d7b6d483204731c573
Adrian Chong <adrianchong518@gmail.com>
2019-09-01 16:04:37 +0800

    Tested Directory Monitoring

--- src/main.py
+++ src/main.py
@@ -1,5 +1,39 @@
+import time
+
+import watchdog.events
+import watchdog.observers
+
+
+TRACKING_FOLDER = "/Users/adrianchong/Downloads"
+
+
+class EventHandler(watchdog.events.FileSystemEventHandler):
+    def on_created(self, event):
+        print(f"Created: {event.src_path}")
+
+    def on_deleted(self, event):
+        print(f"Deleted: {event.src_path}")
+
+    def on_modified(self, event):
+        print(f"Modifed: {event.src_path}")
+
+    def on_moved(self, event):
+        print(f"Moved  : {event.src_path} -> {event.dest_path}")
+
+
 def main():
-  pass
+    eventHander = EventHandler()
+    observer = watchdog.observers.Observer()
+    observer.schedule(eventHander, TRACKING_FOLDER, recursive=True)
+
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+        observer.join()
+
 
 if __name__ == "__main__":
-  main()
+    main()
